a5969da6477b953d50cd728c82636c9fd0a1f029
Hilman Fikry <hilman12fikry@gmail.com>
2025-09-02 10:59:36 +0700

    feat: add form validators

--- src/controllers/indexControllers.js
+++ src/controllers/indexControllers.js
@@ -1,5 +1,16 @@
+const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
 const formatTime = require("../utils/format-time");
+
+const validateMessage = [
+  body("message")
+    .isLength({ min: 1, max: 200 })
+    .withMessage("Message must be between 1 and 200 characters"),
+  body("author")
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .withMessage("Name must be between 1 and 20 characters"),
+];
 
 async function getMessages(_, res) {
   const messages = await db.getAllMessages();
@@ -15,14 +26,25 @@ async function getForm(_, res) {
   res.render("main-layout", { page: "form", title: "New message" });
 }
 
-async function postForm(req, res) {
-  await db.insertMessage({
-    text: req.body.message,
-    user: req.body.author,
-    added: new Date().toISOString(),
-  });
-  res.redirect("/");
-}
+const postForm = [
+  validateMessage,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("main-layout", {
+        page: "form",
+        title: "New message",
+        errors: errors.array(),
+      });
+    }
+    await db.insertMessage({
+      text: req.body.message,
+      user: req.body.author,
+      added: new Date().toISOString(),
+    });
+    res.redirect("/");
+  },
+];
 
 async function getDetails(req, res) {
   const message = await db.getMessage(req.params.messageId);
